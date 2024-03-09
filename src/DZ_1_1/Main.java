6003f2470ea14c0f2c25185a4891811df4da73b5
Sergey-Nov <takalik19941994@gmail.com>
2024-03-09 19:59:29 +0300

    Выполнено ДЗ 1.1 и ДЗ 1.2

---
+++ src/DZ_1_1/Main.java
@@ -0,0 +1,14 @@
+package DZ_1_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int num = scan.nextInt();
+        for (int i=1; i<11; i++){
+            System.out.println(num + "x" + i + "=" + num * i);
+                }
+            }
+        }
---
+++ src/DZ_1_2/Factorial.java
@@ -0,0 +1,18 @@
+package DZ_1_2;
+
+import java.util.Scanner;
+
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int num = scanner.nextInt();
+        int counter = 1;
+            for (int i = 1; i <= num; i++){
+
+                System.out.println(counter*=i);
+            }
+
+         }
+}
