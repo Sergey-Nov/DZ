6f4a65c0fd0e229d8bf93ca54d0bd6c3b38fe1c5
Sergey-Nov <takalik19941994@gmail.com>
2024-03-11 19:45:12 +0300

    Выполнено ДЗ 1 пункт 2.2

--- src/DZ_1_1_1/Main.java
+++ src/DZ_1_1_1/Main.java
@@ -1,14 +1,15 @@
 package DZ_1_1_1;
 
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Input a number: ");
-        int num = scan.nextInt();
-        for (int i=1; i<11; i++){
-            System.out.println(num + "x" + i + "=" + num * i);
+
+        for (int i=1; i<=10; i++){
+            for (int j = 1; j<=10;j++){
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
                 }
             }
         }
---
+++ src/DZ_1_3/Fruit.java
@@ -0,0 +1,32 @@
+package DZ_1_3;
+
+
+
+public class Fruit {
+
+        String name;
+        int price;
+        String sale;
+
+
+
+    public Fruit(String name, int price, String sale){
+            this.name = name;
+            this.price = price;
+            this.sale = sale;
+
+        }
+    public String toString(){
+        return name + ", цена " + price + "руб, " + sale;
+    }
+    public static void main(String[] args) {
+            Fruit apple = new Fruit("Яблоко", 3, "В наличии");
+            Fruit orange = new Fruit("Апельсин", 5, "В наличии");
+            Fruit banana = new Fruit("Банан", 4, "Нет в наличии");
+
+        Object[] fruit = new Object[]{apple,orange,banana};
+        for (int i = 0;i < fruit.length;i++){
+        System.out.println(fruit[i].toString());}
+
+    }
+}
