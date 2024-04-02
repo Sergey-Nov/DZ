8dd0f7aa9083650f133b1996bc24f060d0c66340
Sergey-Nov <takalik19941994@gmail.com>
2024-04-02 16:16:32 +0300

    Add HomeWork 4.3

--- src/HomeWork4/DZ_4_3/MyArrayList.java
+++ src/HomeWork4/DZ_4_3/MyArrayList.java
@@ -1,77 +1,87 @@
 package HomeWork4.DZ_4_3;
 
 
+import java.util.Arrays;
+
 public class MyArrayList {
-    private int capacity = 10;
-    private Object[] element;
+    private static int defaultCapacity = 10;
+    private Object[] elements;
+    private int count;
 
-    public MyArrayList(int capacity) {
-        this.capacity = capacity;
-
+    public MyArrayList() {
+        this.elements =  new Object[defaultCapacity];
+        this.count = 0;
     }
+    private void increaseCapacity(){
 
-    public int getCapacity() {
-        return capacity;
-    }
+            Object[] temp = elements;
+            int newCapacity = defaultCapacity*2;
+            defaultCapacity = newCapacity;
+            elements =  new Object[newCapacity];
+            for (int i = 0; i < temp.length; i++) {
+                elements[i] = temp[i];
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public Object[] getElement() {
-        return element;
-    }
-
-    public void setElement(Object[] element) {
-        this.element = element;
-    }
-    public boolean add(Object object){
-
-        if (capacity>this.element.length){
-            for (int i = 0; i < element.length; i++) {
-                if (element[i]==null){
-                    this.element[i] = object;
-                }else {
-                    i++;
-                }
-            }
-        }else {
-            capacity *= 2;
         }
-    return true;
     }
-    private boolean addArr(Object[] objects){
-        if (capacity!=element.length){
-            for (int i = 0; i < element.length; i++) {
-                if (element[i]==null){
-                    this.element[i] = objects;
-                }else {
-                    i++;
-                }
-            }
-        }else {
-            capacity *= 2;
+    private void trimToSize(){
+        defaultCapacity = 10;
+        elements = new Object[defaultCapacity];
+    }
+    public void addElements(Object o){
+            this.elements[count] = o;
+            count++;
+        if(count == elements.length){
+            increaseCapacity();
         }
-        return true;
     }
-    private boolean addToIndex(Object object, int index){
-        if (capacity!= element.length){
-            for (int i = index; i < element.length ; i++) {
-                if (element[index]==null){
-                    this.element[index]=object;
-                }else if (element[index]!=null&element[index+1]==null){
-                     Object swap;
-                     swap = element[index];
-                     element[index] = object;
-                     element[index + 1]=swap;
+
+    public void addArrElements(Object[] objects){
+            for (int i = 0; i < objects.length; i++) {
+                if(count == elements.length){
+                    increaseCapacity();
                 }
+                this.elements[count] =  objects[i];
+                count ++;
             }
+    }
+
+    public void addToIndex(Object o, int index){
+        if (this.elements[index]==null){
+            this.elements[index]=o;
+        } else if (this.elements[index]!=null && this.elements[index+1]==null) {
+            this.elements[index+1]=elements[index];
+            this.elements[index]=o;
         }else{
-            capacity *= 2;
+            Object[] swap = new Object[defaultCapacity];
+            for (int i = index; i < elements.length ; i++) {
+                swap[i] = elements[i];
+            }
+            this.elements[index]=o;
+            for (int i = index + 1; i < elements.length; i++) {
+                this.elements[i]=swap[i-1];
+            }
         }
-        return true;
+    }
+    public void removeToIndex(int index){
+        Object [] remove = new Object[defaultCapacity];
+        for (int i = index + 1; i < elements.length; i++) {
+            remove[i] = this.elements[i];
+        }
+        for (int i = index; i < elements.length - 1; i++) {
+            this.elements[i] = remove[i + 1];
+        }
+    }
+
+    public void removeAllElements(){
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        trimToSize();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(elements);
     }
 
 }
-
-
--- src/HomeWork4/DZ_4_3/Programm.java
+++ src/HomeWork4/DZ_4_3/Programm.java
@@ -1,11 +1,34 @@
 package HomeWork4.DZ_4_3;
 
+
+
 public class Programm {
     public static void main(String[] args) {
-        String str = new String("abc");
-        MyArrayList arr = new MyArrayList(10);
-        arr.add(str);
-        System.out.println(arr);
-
+        Integer[] integers = new Integer[]{10,15,51,32,31};
+        String[] strings = new String[]{"Март","Апрель","Май","Июнь"};
+        MyArrayList ddd = new MyArrayList();
+        ddd.addArrElements(strings);
+        ddd.addElements(1);
+        ddd.addElements(2);
+        ddd.addArrElements(strings);
+        ddd.addArrElements(strings);
+        ddd.addArrElements(strings);
+        ddd.addArrElements(strings);
+        ddd.addArrElements(strings);
+        ddd.addElements(3);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addArrElements(integers);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addElements(4);
+        ddd.addToIndex(178,0);
+        ddd.removeToIndex(0);
+        ddd.removeAllElements();
+        System.out.println(ddd);
     }
 }
